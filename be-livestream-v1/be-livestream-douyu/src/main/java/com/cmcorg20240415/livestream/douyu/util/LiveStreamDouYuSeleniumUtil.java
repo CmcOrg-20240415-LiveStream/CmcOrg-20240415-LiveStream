f2d76d5e9e5ad91176cb8c2e7ccc54b1d2f4cdda
Cmc0 <dimlt@vip.qq.com>
2024-04-15 21:37:49 +0800

    【i】【优化】【代码】

---
+++ be-livestream-v1/be-livestream-douyu/src/main/java/com/cmcorg20240415/livestream/douyu/util/LiveStreamDouYuSeleniumUtil.java
@@ -0,0 +1,15 @@
+package com.cmcorg20240415.livestream.douyu.util;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class LiveStreamDouYuSeleniumUtil {
+
+    @PostConstruct
+    public void postConstruct() {
+
+    }
+
+}
